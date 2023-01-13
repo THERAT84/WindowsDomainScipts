6bc60949fb8dfaecd4db637c72813927b2f75be0
THERAT84 <59366848+THERAT84@users.noreply.github.com>
2023-01-13 16:18:30 +0100

    Update createfolders.ps1

--- createfolders.ps1
+++ createfolders.ps1
@@ -1,5 +1,17 @@
+<#########################################################################
+Skriptname:     createfolders.ps1
+Funktion:       Erstellung Ordnerstruktur mit Textdatei
+Erstellt am:    13.1.2023
+Author:         N.Viragh
+Version:        1.0
+Aenderungen:
+#>
+##########################################################################
+#Einlesen txt Datei
+#Ordner in TXT müssen mit \ getrennt werden
+
 $folders = Get-Content "C:\bla\bla\tmp\create_folders.txt"
 foreach ($folder in $folders) {
-    # Create the main folder
-    New-Item -ItemType Directory -Path "D:\bla\bla\$folder" 
+    
+    New-Item -ItemType Directory -Path "D:\DFS\Kundenname\$folder" 
 }
