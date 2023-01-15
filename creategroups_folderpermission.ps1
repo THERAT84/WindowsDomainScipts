9c2ad3a05b4445e2eee4934eda8c9b63e8cda643
THERAT84 <nico@nv-systems.ch>
2023-01-15 10:46:07 +0100

    edited script

--- creategroups_folderpermission.ps1
+++ creategroups_folderpermission.ps1
@@ -11,7 +11,7 @@ Modifications:
 #Variablen deklarieren
 # Mögliche Gruppenpfade -> Letzte OU
 # Systemgruppen","Dateiberechtigungsgruppen","Druckerzuweisung","Softwaregruppen","Outlookberechtigungsgruppen","Verteilerliste
-$grouppath = "OU=Dateiberechtigungen,OU=,OU=Rebstein,OU='Elektro Kleiner AG',DC=elektrokleiner,DC=local"
+$grouppath = "OU=Dateiberechtigungsgruppen,OU=Gruppen,OU=Global,OU=Elektro Kleiner AG,DC=elektrokleiner,DC=local"
 
 $groups = Get-Content "C:\scripts\files\create_groups.txt"
 $prefix = "GG_"
@@ -20,6 +20,6 @@ $sufix_RW = "_RW"
 
 foreach ($group in $groups) 
 { 
-    New-ADGroup -Name $prefix+$group+$sufix_R -SamAccountName $group -GroupCategory Security -GroupScope Global -DisplayName $group -Path $grouppath
-    New-ADGroup -Name $prefix+$group+$sufix_RW -SamAccountName $group -GroupCategory Security -GroupScope Global -DisplayName $group -Path $grouppath  
+    New-ADGroup -Name $prefix+$group+$sufix_R -SamAccountName $prefix+$group+$sufix_R -GroupCategory Security -GroupScope Global -DisplayName $prefix+$group+$sufix_R -Path $grouppath
+    New-ADGroup -Name $prefix+$group+$sufix_RW -SamAccountName $prefix+$group+$sufix_RW -GroupCategory Security -GroupScope Global -DisplayName $prefix+$group+$sufix_RW -Path $grouppath  
 }
