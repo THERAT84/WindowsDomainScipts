2f94cbdb0d0a52953773d362bc664421b0f4ff5c
THERAT84 <nico@nv-systems.ch>
2023-02-15 16:48:43 +0100

    script changed

--- yearfolder.ps1
+++ yearfolder.ps1
@@ -1,6 +1,6 @@
 $years = @(2002..2023)
 $LW = "D:\"
-$Basepath = "DFS-Datenpool\Elektro Kleiner AG"
+$Basepath = "DFS-Datenpool\Kundenname"
 $path = @("021_Personalwesen\Taggelder")
 $Quartal = @("1.Quartal","2.Quartal","3.Quartal","4.Quartal")
 $TG = @("EO","Unfall","Krankheit","MSE")
