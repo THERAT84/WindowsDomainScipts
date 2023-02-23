7854b83edd88197ce13fcda8a3443b263408f78f
Nico Viragh <nvra@sfs.com>
2023-02-23 11:06:06 +0100

    Added delete script

---
+++ delete_user.ps1
@@ -0,0 +1,58 @@
+<#########################################################################
+Scriptname:     delete_user.ps1
+Function:       Delete user from AD, Exchange and remove 
+                home, profile and usershared folders
+Created at:     23.02.2023
+Author:         THERAT84
+Version:        1.0
+Modifications:
+#>
+##########################################################################
+
+$username = Read-Host "Enter loginname of the user to delete"
+
+foreach ($user in $username)
+{
+    $profilepath = Get-ADUser -Identity $user -Properties ProfilePath | Format-Table -Property ProfilePath -HideTableHeaders | Out-String
+    $homepath =  Get-ADUser -Identity $user -Properties HomeDirectory | Format-Table -Property HomeDirectory -HideTableHeaders | Out-String
+    #$usfpath =
+    # Convert SMB Share Name to real path of the folders
+    if ($profilepath -like '*profil*')
+    {
+       $realProfilPath = Get-SmbShare -Name "profil*" | Format-Table -Property Path -HideTableHeaders
+    }
+    if ($homepath -like '*home*')
+    {
+        $realHomePath = Get-SmbShare -name "home*" | Format-Table -Property Path -HideTableHeaders
+    }
+        ForEach-Object($pdir in $realProfilPath)
+        {
+            $checkprofilepath = Test-Path $pdir
+                if ($checkprofilepath -eq $true)
+                {
+                    Set-Location -Path $pdir
+                    Remove-Item -Path $pdir -Force -Recurse 
+                    Write-Host "$name Profilefolder at $pdir deleted" -BackgroundColor Green
+                }
+                else 
+                {
+                    Write-Host "$name Profile Folder doesnt exist" -BackgroundColor Red
+                }
+        }
+        ForEach-Object($hdir in $realHomePath)
+        {
+            $checkhomepath = Test-Path $hdir
+                if ($checkhomepath -eq $true)
+                {
+                    Set-Location -Path $hdir
+                    Remove-Item -Path $hdir -Force -Recurse
+                    Write-Host "$name Homefolder at $hdir deleted" -BackgroundColor Green
+                }
+                else
+                {
+                    Write-Host "$name Home Folder doesnt exist" -BackgroundColor Red
+                }
+        }
+    Remove-ADUser -Identity $user 
+    Write-Host "$name User deleted" -BackgroundColor Green
+}
