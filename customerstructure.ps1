1202a28b9ce37b1ac8d61e20f8242c119d140e26
THERAT84 <59366848+THERAT84@users.noreply.github.com>
2025-03-12 16:57:33 +0100

    Create customerstructure.ps1

---
+++ customerstructure.ps1
@@ -0,0 +1,9 @@
+<#########################################################################
+Scriptname:     customerstructure.ps1
+Function:       creates a skeletionstructure for each customerfolder
+Created at:     12.03.2025
+Author:         THERAT84
+Version:        1.0
+Modifications:
+#>
+##########################################################################
